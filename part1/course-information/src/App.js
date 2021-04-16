6f8e586dcb534f3c9b8698c77c2d4bd2bf57b8bb
S0lRaK <carlosj.gc989@gmail.com>
2021-04-16 11:04:44 -0600

    refactor: :recycle: Exercise 1.2
    Course information, step2

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+const Header = ({ course }) => <h1>{course}</h1>
+
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
   )
 }
-const Total = (props) => {
-  return <p>Number of exercises: {props.exercises}</p>
-}
+
+const Part = ({part, exercises}) => <p>{part} {exercises}</p>
+
+const Total = ({ exercises }) => <p>Number of exercises: {exercises}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -26,9 +28,14 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </>
   )
