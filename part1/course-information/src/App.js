509af508c4ba36cc0b6c6e16f753560889f37799
Carlos J. García Carmona <carlos@carlosjgc.me>
2022-01-31 15:18:14 -0700

    refactor: :recycle: Exercise 1.3
    Using objects

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -5,26 +5,32 @@ const Header = ({ course }) => <h1>{course}</h1>
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </>
   )
 }
 
-const Part = ({part, exercises}) => <p>{part} {exercises}</p>
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Total = ({ exercises }) => <p>Number of exercises: {exercises}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
   return (
     <>
       <Header course={course} />
@@ -32,11 +38,8 @@ const App = () => {
         part1={part1}
         part2={part2}
         part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
       />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 }
