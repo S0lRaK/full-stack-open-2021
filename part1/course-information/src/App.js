ad75ae5d15ea80a74ca66b426271eff9b7d3be23
Carlos J. García Carmona <carlos@carlosjgc.me>
2022-01-31 17:04:36 -0700

    refactor: :recycle: Exercise 1.4
    Array of objects

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -2,44 +2,51 @@ import React from 'react'
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </>
   )
 }
 
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
 
-const Total = ({ exercises }) => <p>Number of exercises: {exercises}</p>
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises:{' '}
+    {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
+)
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
+
   return (
     <>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
