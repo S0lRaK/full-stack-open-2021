025079554833a2f7aa26182de27084a948f11a58
Carlos J. García Carmona <carlos@carlosjgc.me>
2022-02-08 17:39:24 -0700

    chore: :construction: Add deployment scripts and update Vite build base for Anecdotes app

--- part1/anecdotes/vite.config.js
+++ part1/anecdotes/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/full-stack-open-2021/'
+  base: '/full-stack-open-2021/part1/anecdotes/dist/',
 })
